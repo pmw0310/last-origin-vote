da7da4a0ebcc7b8e6969344d8196e27e2fefb616
pmw031 <pmw0310@knou.ac.kr>
2020-09-09 21:56:34 +0900

    fix ts

--- server/index.ts
+++ server/index.ts
@@ -6,54 +6,54 @@ import { ApolloServer, gql } from "apollo-server-koa";
 import next from "next";
 
 const port = parseInt(process.env.PORT || "4000", 10);
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 function renderNext(route: string) {
-  return (ctx: any) => {
-    ctx.res.statusCode = 200;
-    ctx.respond = false;
+	return (ctx: Koa.Context) => {
+		ctx.res.statusCode = 200;
+		ctx.respond = false;
 
-    app.render(ctx.req, ctx.res, route, {
-      ...((ctx.request && ctx.request.body) || {}),
-      ...ctx.params,
-      ...ctx.query,
-    });
-  };
+		app.render(ctx.req, ctx.res, route, {
+			...((ctx.request && ctx.request.body) || {}),
+			...ctx.params,
+			...ctx.query,
+		});
+	};
 }
 
 const typeDefs = gql`
-  type Query {
-    hello: String
-  }
+	type Query {
+		hello: String
+	}
 `;
- 
+
 const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
+	Query: {
+		hello: () => "Hello world!",
+	},
 };
 
 app.prepare().then(() => {
-  const server = new Koa()
-  const router = new Router()
-  const apolloServer = new ApolloServer({ typeDefs, resolvers });
-  apolloServer.applyMiddleware({app: server});
+	const server = new Koa();
+	const router = new Router();
+	const apolloServer = new ApolloServer({ typeDefs, resolvers });
+	apolloServer.applyMiddleware({ app: server });
 
-  router.get("/", renderNext("/"));
+	router.get("/", renderNext("/"));
 
-  server
-    .use(morgan("combined"))
-    .use(
-      mount("/", (ctx: Koa.Context) => {
-        ctx.respond = false;
-        handle(ctx.req, ctx.res);
-      })
-    );
+	server.use(morgan("combined")).use(
+		mount("/", (ctx: Koa.Context) => {
+			ctx.respond = false;
+			handle(ctx.req, ctx.res);
+		}),
+	);
 
-  server.listen(port, () => {
-    console.log(`> Next on http://localhost:${port}`)
-    console.log(`> GraphQL on http://localhost:${port}${apolloServer.graphqlPath}`)
-  })
-})
+	server.listen(port, () => {
+		console.log(`> Next on http://localhost:${port}`);
+		console.log(
+			`> GraphQL on http://localhost:${port}${apolloServer.graphqlPath}`,
+		);
+	});
+});
