8a1a42d11abd02603165e15823384f02eb33c77e
pmw0310 <mw.park@retigrid.com>
2020-09-11 14:44:46 +0900

    dev

--- server/index.ts
+++ server/index.ts
@@ -3,13 +3,17 @@ import morgan from 'koa-morgan';
 import mount from 'koa-mount';
 import Router from 'koa-router';
 import Bodyparser from 'koa-bodyparser';
+// import proxy from 'koa-proxies';
 import helmet from 'koa-helmet';
+import passport from 'koa-passport';
+import { Strategy, Profile } from 'passport-naver';
 import { ApolloServer, gql } from 'apollo-server-koa';
+import mongoose from 'mongoose';
 import next from 'next';
-import { readFileSync } from 'fs';
+// import { readFileSync } from 'fs';
 
-const t = readFileSync(`${__dirname}/t.txt`, 'utf-8');
-console.log(t);
+// const t = readFileSync(`${__dirname}/t.json`, 'utf-8');
+// console.log(t);
 
 const port = parseInt(process.env.PORT || '4000', 10);
 const dev = process.env.NODE_ENV !== 'production';
@@ -47,11 +51,47 @@ app.prepare().then(() => {
 	const apolloServer = new ApolloServer({ typeDefs, resolvers });
 	apolloServer.applyMiddleware({ app: server });
 
+	passport.use(
+		new Strategy(
+			{
+				clientID: process.env.NAVER_CLIENT_ID as string,
+				clientSecret: process.env.NAVER_CLIENT_SECRET as string,
+				callbackURL: `http://localhost:${port}/naver/callback`,
+			},
+			(
+				accessToken: string,
+				refreshToken: string,
+				profile: Profile,
+				done: (error: null, user?: null) => void,
+			): void => {
+				console.log(accessToken);
+				console.log(refreshToken);
+				console.log(profile);
+
+				return done(null, null);
+			},
+		),
+	);
+
+	mongoose
+		.connect(process.env.MONGO_URI as string, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		})
+		.then(() => {
+			console.log('Connected to MongoDB');
+		})
+		.catch((e) => {
+			console.error(e);
+		});
+
 	router.get('/', renderNext('/'));
+	router.get('/naver', passport.authenticate('naver', {}));
 
 	server
 		.use(Bodyparser())
 		.use(helmet())
+		.use(passport.initialize())
 		.use(morgan('combined'))
 		.use(
 			mount('/', (ctx: Koa.Context) => {
