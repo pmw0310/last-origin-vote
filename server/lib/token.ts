d4fcb7a0fb634516f05e26da164223db54b68736
pmw031 <pmw0310@knou.ac.kr>
2020-09-13 21:16:50 +0900

    naver 로그인 구형

---
+++ server/lib/token.ts
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import { Profile } from 'passport-naver';
+
+export function generateToken(payload: Profile): Promise<string> {
+	return new Promise((resolve, reject) => {
+		const { id } = payload;
+		jwt.sign(
+			{ id },
+			process.env.JWT_SECRET as string,
+			{
+				expiresIn: '12h',
+			},
+			(error, token) => {
+				if (error) reject(error);
+				resolve(token);
+			},
+		);
+	});
+}
