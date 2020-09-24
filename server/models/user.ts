24bb4f8a26a5969ad4ab7671a328d44dfb41bfe6
pmw0310 <mw.park@retigrid.com>
2020-09-24 18:20:07 +0900

    보안설정

--- server/models/user.ts
+++ server/models/user.ts
@@ -2,8 +2,8 @@ import { Context } from 'koa';
 import { Document, Schema, model, connection, Model } from 'mongoose';
 import autoIncrement from 'mongoose-auto-increment';
 import { uid } from 'rand-token';
-import bcrypt from 'bcrypt';
 import jwt, { VerifyErrors } from 'jsonwebtoken';
+import aes from '../lib/aes256cbc';
 
 export interface UserTypeModel extends Document {
     _id: string;
@@ -13,8 +13,8 @@ export interface UserTypeModel extends Document {
     profileImage?: string;
     createdAt: number;
     authority: string[];
-    generateAccessToken: (ctx?: Context) => string;
-    generateRefreshToken: (ctx?: Context) => Promise<string>;
+    generateAccessToken: (ctx: Context) => string;
+    generateRefreshToken: (ctx: Context) => Promise<string>;
 }
 
 export interface UserStaticsModel extends Model<UserTypeModel> {
@@ -28,7 +28,11 @@ const UserSchema = new Schema<UserTypeModel>({
         unique: true,
         index: true,
     },
-    token: String,
+    token: {
+        type: String,
+        unique: true,
+        index: true,
+    },
     nickname: String,
     profileImage: String,
     createdAt: {
@@ -44,16 +48,18 @@ const UserSchema = new Schema<UserTypeModel>({
     },
 });
 
-UserSchema.methods.generateAccessToken = function (ctx?: Context): string {
-    const token = jwt.sign(
-        {
-            _id: this._id,
-            uid: this.uid,
-        },
-        process.env.JWT_SECRET as string,
-        {
-            expiresIn: 60 * 10,
-        },
+UserSchema.methods.generateAccessToken = function (ctx: Context): string {
+    const token = aes.encrypt(
+        jwt.sign(
+            {
+                _id: this._id,
+                uid: this.uid,
+            },
+            process.env.JWT_SECRET as string,
+            {
+                expiresIn: 60 * 10,
+            },
+        ),
     );
 
     if (ctx) {
@@ -63,125 +69,133 @@ UserSchema.methods.generateAccessToken = function (ctx?: Context): string {
         });
     }
 
-    return token;
+    return t;
 };
 
 UserSchema.methods.generateRefreshToken = async function (
-    ctx?: Context,
+    ctx: Context,
 ): Promise<string> {
     this.token = uid(16);
     await this.save();
 
-    const token = jwt.sign(
-        {
-            token: this.token,
-            key: await bcrypt.hash(this.createdAt.toString(), 10),
-        },
-        process.env.JWT_SECRET as string,
-        {
-            expiresIn: '14d',
-        },
-    );
-
     if (ctx) {
-        ctx.cookies.set('refresh_token', token, {
+        ctx.cookies.set('refresh_token', this.token, {
             httpOnly: true,
             maxAge: 1000 * 60 * 60 * 24 * 14,
         });
     }
 
-    return token;
+    return this.token;
 };
 
 UserSchema.statics.verify = async function (ctx: Context) {
-    const accessToken = ctx.cookies.get('access_token') as string;
-    const refreshToken = ctx.cookies.get('refresh_token') as string;
+    const accessToken = ctx.cookies.get('access_token');
+    const refreshToken = ctx.cookies.get('refresh_token');
 
-    type JwtVerify = {
+    type JwtVerify<T> = {
         error: VerifyErrors | null;
-        decoded: User | Refresh | undefined;
+        decoded: T | undefined;
     };
 
-    type User = {
+    type Access = {
         _id: string;
         uid: number;
+        exp: number;
     };
 
     type Refresh = {
         token: string;
         key: string;
-        exp: number;
     };
 
-    const { _id: userId, uid: userUid } = jwt.decode(accessToken) as User;
-    const user = await this.findOne({ _id: userId });
-
-    const jwtVerify = (token: string, secret: string): Promise<JwtVerify> => {
+    const jwtVerify = <T>(
+        token: string,
+        secret: string,
+    ): Promise<JwtVerify<T>> => {
         return new Promise((resolve) => {
             jwt.verify(token, secret, (error, decoded) => {
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                return resolve({ error, decoded: decoded as any });
+                return resolve({ error, decoded: decoded as T | undefined });
             });
         });
     };
 
-    const accessTokenVerify = await jwtVerify(
-        accessToken,
-        process.env.JWT_SECRET as string,
-    );
-
-    if (accessTokenVerify.error) {
-        if (accessTokenVerify.error.name === 'TokenExpiredError') {
-            if (userId !== user._id || userUid !== user.uid) {
-                return { error: 403, user: undefined };
-            }
-
-            const refreshTokenVerify = await jwtVerify(
-                refreshToken,
+    try {
+        if (accessToken) {
+            const accessTokenVerify = await jwtVerify<Access>(
+                aes.decrypt(accessToken as string),
                 process.env.JWT_SECRET as string,
             );
 
-            if (refreshTokenVerify.error) {
-                if (accessTokenVerify.error.name === 'TokenExpiredError') {
-                    const decode = jwt.decode(refreshToken);
-                    const dc: Refresh = decode as Refresh;
-
-                    if (
-                        dc.token !== user.token ||
-                        !(await bcrypt.compare(
-                            user.createdAt.toString(),
-                            dc.key,
-                        ))
-                    ) {
-                        return { error: 403, user: undefined };
-                    }
-
-                    await user.generateRefreshToken(ctx);
-                }
+            if (accessTokenVerify.error) {
+                return;
             }
 
-            const now = Math.floor(Date.now() * 0.001);
-            if (
-                (refreshTokenVerify.decoded as Refresh).exp - now <
-                60 * 60 * 24
-            ) {
-                await user.generateRefreshToken(ctx);
-            }
+            const { _id, uid } = accessTokenVerify.decoded as Access;
 
+            const user = await this.findOne({ _id, uid });
+        } else if (refreshToken) {
+            const user = await this.findOne({ token: refreshToken as string });
             user.generateAccessToken(ctx);
-
-            return { error: undefined, user };
         }
-        return { error: 403, user: undefined };
+    } catch (e) {
+        return;
     }
 
-    const dc: User = accessTokenVerify.decoded as User;
+    // const { _id: userId, uid: userUid } = jwt.decode(accessToken) as User;
+    // const user = await this.findOne({ _id: userId });
 
-    if (dc._id === user._id && dc.uid === user.uid) {
-        return { error: undefined, user };
-    }
+    // if (accessTokenVerify.error) {
+    //     if (accessTokenVerify.error.name === 'TokenExpiredError') {
+    //         if (userId !== user._id || userUid !== user.uid) {
+    //             return { error: 403, user: undefined };
+    //         }
 
-    return { error: 403, user: undefined };
+    //         const refreshTokenVerify = await jwtVerify(
+    //             refreshToken,
+    //             process.env.JWT_SECRET as string,
+    //         );
+
+    //         if (refreshTokenVerify.error) {
+    //             if (accessTokenVerify.error.name === 'TokenExpiredError') {
+    //                 const decode = jwt.decode(refreshToken);
+    //                 const dc: Refresh = decode as Refresh;
+
+    //                 if (
+    //                     dc.token !== user.token ||
+    //                     !(await bcrypt.compare(
+    //                         user.createdAt.toString(),
+    //                         dc.key,
+    //                     ))
+    //                 ) {
+    //                     return { error: 403, user: undefined };
+    //                 }
+
+    //                 await user.generateRefreshToken(ctx);
+    //             }
+    //         }
+
+    //         const now = Math.floor(Date.now() * 0.001);
+    //         if (
+    //             (refreshTokenVerify.decoded as Refresh).exp - now <
+    //             60 * 60 * 24
+    //         ) {
+    //             await user.generateRefreshToken(ctx);
+    //         }
+
+    //         user.generateAccessToken(ctx);
+
+    //         return { error: undefined, user };
+    //     }
+    //     return { error: 403, user: undefined };
+    // }
+
+    // const dc: User = accessTokenVerify.decoded as User;
+
+    // if (dc._id === user._id && dc.uid === user.uid) {
+    //     return { error: undefined, user };
+    // }
+
+    // return { error: 403, user: undefined };
 };
 
 export default model<UserTypeModel, UserStaticsModel>('User', UserSchema);
