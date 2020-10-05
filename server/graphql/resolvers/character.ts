45128d75e2adbc1a7755f6cf0403acd036f178ec
pmw0310 <mw.park@retigrid.com>
2020-10-05 13:47:22 +0900

    케릭터 작업중2

--- server/graphql/resolvers/character.ts
+++ server/graphql/resolvers/character.ts
@@ -1,9 +1,11 @@
 import {
     Resolver,
     Query,
-    // Mutation,
+    Mutation,
     Field,
+    // InterfaceType,
     ObjectType,
+    InputType,
     Int,
     // Authorized,
     registerEnumType,
@@ -30,66 +32,82 @@ registerEnumType(CharacterRole, {
     description: '케릭터 역할',
 });
 
-@ObjectType({ description: '케릭터 데이터' })
+@InputType('CharacterDataInput')
+@ObjectType('CharacterDataObject')
 export class CharacterData {
     @Field(() => Int, {
         description: '번호',
+        nullable: true,
     })
     number?: number;
     @Field({
         description: '부대',
+        nullable: true,
     })
     unit?: string;
     @Field(() => CharacterGrade, {
         description: '등급',
+        nullable: true,
     })
     grade?: CharacterGrade;
     @Field(() => CharacterGrade, {
         description: '최종 등급',
+        nullable: true,
     })
     lastGrade?: CharacterGrade;
     @Field(() => CharacterType, {
         description: '타입',
+        nullable: true,
     })
     type?: CharacterType;
     @Field(() => CharacterRole, {
         description: '역할',
+        nullable: true,
     })
     role?: CharacterRole;
     @Field({
         description: '클레스',
+        nullable: true,
     })
     class?: string;
     @Field({
         description: '무장',
+        nullable: true,
     })
     arm?: string;
     @Field(() => Int, {
         description: '신장',
+        nullable: true,
     })
     stature?: number;
     @Field(() => Int, {
         description: '체중',
+        nullable: true,
     })
     weight?: number;
     @Field({
         description: '설명',
+        nullable: true,
     })
     description?: string;
 }
 
-@ObjectType({ description: '케릭터 정보' })
+@InputType('CharacterInput')
+@ObjectType('CharacterObject')
 export class Character {
     @Field({
         description: '이름',
+        nullable: true,
     })
     name?: string;
     @Field({
         description: '프로필 url',
+        nullable: true,
     })
     profileImage?: string;
     @Field({
         description: '생성 날짜',
+        nullable: true,
     })
     createdAt?: Date;
     @Field(() => [String], {
@@ -99,6 +117,7 @@ export class Character {
     tag?: string[];
     @Field(() => CharacterData, {
         description: '케릭터 데이터',
+        nullable: true,
     })
     data?: CharacterData;
 }
@@ -110,24 +129,24 @@ export default class CharacterResolver {
         @Arg('page', () => Int, { nullable: true }) page: number = 1,
         @Arg('limit', () => Int, { nullable: true }) limit: number = 15,
     ): Promise<Character[]> {
-        return await CharacterModels.find()
+        const char = await CharacterModels.find()
             .sort({ _id: -1 })
             .limit(limit)
             .skip((page - 1) * limit)
             .lean()
             .exec();
+
+        return char as Character[];
     }
 
-    // @Mutation(() => Boolean)
-    // async addCharacter(
-    //     @Arg('data', () => Character) data: Character,
-    // ): Promise<boolean> {
-    //     try {
-    //         const char = new CharacterModels(data);
-    //         await char.save();
-    //         return true;
-    //     } catch (e) {
-    //         return false;
-    //     }
-    // }
+    @Mutation(() => Boolean)
+    async addCharacter(@Arg('data') data: Character): Promise<boolean> {
+        try {
+            const char = new CharacterModels(data);
+            await char.save();
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
 }
