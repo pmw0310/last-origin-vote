46cd0ff8415a3ed5000fae7fad0bee80bf43020f
pmw0310 <mw.park@retigrid.com>
2020-10-23 13:03:22 +0900

    리스트아이템 작업중

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState } from 'react';
 import Link from 'next/link';
-import { Paper, Typography, IconButton, Chip, Avatar } from '@material-ui/core';
+import {
+    Typography,
+    IconButton,
+    Chip,
+    Avatar,
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+} from '@material-ui/core';
 import AvatarGroup from '@material-ui/lab/AvatarGroup';
 import styled from 'styled-components';
 import { CharacterInterface, GroupInterface } from 'Module';
-import { Edit, Delete } from '@material-ui/icons';
+import { Edit, Delete, ExpandMore as ExpandMoreIcon } from '@material-ui/icons';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 export interface CharacterItemProps {
@@ -14,16 +23,21 @@ export interface CharacterItemProps {
     type: 'char' | 'group';
 }
 
-const ItemPaper = styled(Paper)`
+const Root = styled.div`
     margin: 12px;
     width: auto;
-    height: 150px;
 `;
 
-const Item = styled.div`
-    display: flex;
-    align-items: flex-start;
-`;
+// const ItemPaper = styled(Paper)`
+//     margin: 12px;
+//     width: auto;
+//     height: 150px;
+// `;
+
+// const Item = styled.div`
+//     display: flex;
+//     align-items: flex-start;
+// `;
 
 const Info = styled.div`
     display: flex;
@@ -44,66 +58,87 @@ const ListItem: React.FC<CharacterItemProps> = ({
     removeDialogOpen,
     type,
 }): JSX.Element => {
+    const [expanded, setExpanded] = useState<boolean>(false);
+
+    const handleChangeAccordion = (
+        _event: React.ChangeEvent<any>,
+        expanded: boolean,
+    ): void => {
+        setExpanded(expanded);
+    };
+
     return (
-        <ItemPaper>
-            <Item>
-                <LazyLoadImage
-                    src={
-                        (data as CharacterInterface).profileImage ||
-                        (data as GroupInterface).image ||
-                        'https://via.placeholder.com/150x150.png?text=No+Image'
-                    }
-                    effect="opacity"
-                    width="150"
-                    height="150"
-                />
-                <Info>
-                    <Typography variant="h5" gutterBottom>
-                        {data.name}
-                    </Typography>
-                    <div>
-                        {data.tag?.map((tag, index) => (
-                            <TagChip
-                                variant="outlined"
-                                size="small"
-                                label={`#${tag}`}
-                                key={`${data.name}-${index}`}
-                            />
-                        ))}
-                    </div>
-                </Info>
-                {type === 'group' &&
-                    ((data as GroupInterface).character as Array<
-                        CharacterInterface
-                    >)?.length > 0 && (
-                        <AvatarGroup max={5}>
-                            {((data as GroupInterface).character as Array<
-                                CharacterInterface
-                            >).map((char) => (
-                                <Avatar
-                                    key={char.id}
-                                    alt={char.name}
-                                    src={char.profileImage}
+        <Root>
+            <Accordion expanded={expanded} onChange={handleChangeAccordion}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1c-content"
+                    id="panel1c-header"
+                >
+                    <LazyLoadImage
+                        src={
+                            (data as CharacterInterface).profileImage ||
+                            (data as GroupInterface).image ||
+                            'https://via.placeholder.com/150x150.png?text=No+Image'
+                        }
+                        effect="opacity"
+                        width="150"
+                        height="150"
+                    />
+                    <Info>
+                        <Typography variant="h5" gutterBottom>
+                            {data.name}
+                        </Typography>
+                        <div>
+                            {data.tag?.map((tag, index) => (
+                                <TagChip
+                                    variant="outlined"
+                                    size="small"
+                                    label={`#${tag}`}
+                                    key={`${data.name}-${index}`}
                                 />
                             ))}
-                        </AvatarGroup>
-                    )}
-                {auth && (
-                    <Auth>
-                        <Link href={`/${type}/[id]`} as={`/${type}/${data.id}`}>
-                            <IconButton>
-                                <Edit />
+                        </div>
+                    </Info>
+                    {auth && (
+                        <Auth>
+                            <Link
+                                href={`/${type}/[id]`}
+                                as={`/${type}/${data.id}`}
+                            >
+                                <IconButton>
+                                    <Edit />
+                                </IconButton>
+                            </Link>
+                            <IconButton
+                                onClick={removeDialogOpen(data.id as string)}
+                            >
+                                <Delete />
                             </IconButton>
-                        </Link>
-                        <IconButton
-                            onClick={removeDialogOpen(data.id as string)}
-                        >
-                            <Delete />
-                        </IconButton>
-                    </Auth>
-                )}
-            </Item>
-        </ItemPaper>
+                        </Auth>
+                    )}
+                </AccordionSummary>
+                <AccordionDetails>
+                    {type === 'group' &&
+                        expanded &&
+                        ((data as GroupInterface).character as Array<
+                            CharacterInterface
+                        >)?.length > 0 && (
+                            <AvatarGroup max={5}>
+                                {((data as GroupInterface).character as Array<
+                                    CharacterInterface
+                                >).map((char) => (
+                                    <Avatar
+                                        key={char.id}
+                                        alt={char.name}
+                                        src={char.profileImage}
+                                    />
+                                ))}
+                            </AvatarGroup>
+                        )}
+                </AccordionDetails>
+            </Accordion>
+        </Root>
     );
 };
 
